df0c6b3858c3869a49e76125810c67f17b0cfdd8
Jamie Frost <Jamcris11@hotmail.co.uk>
2020-04-21 16:17:53 +0100

    Alt: now returns filename if prefix plus ricardo is typed
    Signed-off-by: Jamie Frost <Jamcris11@hotmail.co.uk>

--- scripts/PostRicardo.js
+++ scripts/PostRicardo.js
@@ -1,9 +1,11 @@
 var config = require('../config.js');
+var FS = require('./FileFinder.js');
 
 exports.HandleMsg = function(msg) {
 	if (msg.content.toLowerCase() == (config.Prefix + "ricardo")) {
 		console.log('Meme posted!');
-		return 0;
+		var file = FS.FindRandomFile('C:/Users/Home/Desktop/Memes/Ricardo 2', true);
+		return file;
 	}
 	else {
 		return -1;
